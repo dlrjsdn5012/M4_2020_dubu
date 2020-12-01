76d2b020bc069d68bde5955a9d6fc764513e10e1
dlrjsdn5012 <dlrjsdn5012@naver.com>
2020-12-01 12:57:46 +0900

    edit

--- UJ/clone_website/2nd/index.js
+++ UJ/clone_website/2nd/index.js
@@ -6,4 +6,12 @@ window.onload = function () {
         document.getElementById('case3').style.backgroundColor = ('green')
         document.getElementById('footer').style.backgroundColor = ('rgb(170,40,40)')
     });
+
+
+    document.getElementById('XMas2').addEventListener('click', function () {
+        document.getElementById('case1').style.backgroundColor = ('rgb(0, 0, 180)')
+        document.getElementById('case2').style.backgroundColor = ('#000088')
+        document.getElementById('case3').style.backgroundColor = ('rgb(0, 0, 180)')
+        document.getElementById('footer').style.backgroundColor = ('#000088')
+    });
 }
